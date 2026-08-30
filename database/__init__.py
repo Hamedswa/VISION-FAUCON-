982fa856d6246962b2786103518d91927623cd7c
Hamedswa <hamedsawadogo003@gmail.com>
2026-08-30 20:32:47 +0000

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,13 @@
-# data/__init__.py
-from .oanda_client import OandaClient
-from .ccxt_client import CCXTClient
-from .data_manager import DataManager
+# database/__init__.py
+from .models import Base, Signal, Trade, Candle, OptimizerRun, PerformanceSnapshot
+from .db_manager import DatabaseManager
 
-__all__ = ["OandaClient", "CCXTClient", "DataManager"]
+__all__ = [
+    "Base",
+    "Signal",
+    "Trade",
+    "Candle",
+    "OptimizerRun",
+    "PerformanceSnapshot",
+    "DatabaseManager",
+]
